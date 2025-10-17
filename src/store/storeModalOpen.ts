6d8af7cc1fefd1e19b24b5565924ed05ceb6d32b
ivex0002 <garam199803@gmail.com>
2025-10-17 14:12:34 +0900

    fix:스토어 이름변경 및 내부 함수 카멜케이스로 변경

--- src/store/storeModalOpen.ts
+++ src/store/storeModalOpen.ts
@@ -2,9 +2,9 @@ import { create } from 'zustand'
 
 interface ModalOpen {
   isModalOpen: boolean
-  SetModalOpen: (isOpen: boolean) => void
+  setModalOpen: (isOpen: boolean) => void
 }
 export const storeModalOpen = create<ModalOpen>((set) => ({
   isModalOpen: false,
-  SetModalOpen: (isOpen) => set(() => ({ isModalOpen: isOpen })),
+  setModalOpen: (isOpen) => set(() => ({ isModalOpen: isOpen })),
 }))
